5c43cd211db68033205cbb6bd431db5959523244
Влад Соловьёв <solovevvlad70706@gmail.com>
2024-08-22 16:55:33 +0300

    Завдання 1: Програма, яка відображає повідомлення "I love C++"

--- Homework1/Homework1.cpp
+++ Homework1/Homework1.cpp
@@ -1,3 +1,8 @@
-﻿int main() {
-    return 0; // Програма нічого не робить і просто завершується
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I love C++\n"; // Виводимо повідомлення на екран
+    return 0; // Завершуємо програму з кодом 0
+
 }
